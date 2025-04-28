8faac54bab102d80c3730c2561f98e3a2f865ad1
Sirshapan Mitra <sirsh07>
2025-04-28 01:24:49 -0400

    debugged

--- dataset_gen.py
+++ dataset_gen.py
@@ -230,7 +230,7 @@ def sample_and_combine_folders_street(base_dir,
     # import pdb; pdb.set_trace()
     for file_path in tqdm(all_sampled_file_path, desc="Symlinking files to 100p folder"):
         file_name = os.path.basename(file_path)
-        target_file_path = os.path.join(target_100p, file_name.replace("street", ""))
+        target_file_path = os.path.join(target_100p, file_name.replace("_street", ""))
         assert os.path.exists(file_path), f"File not found: {file_path}"
         if not os.path.exists(target_file_path):
             os.symlink(file_path, target_file_path)
@@ -241,7 +241,7 @@ def sample_and_combine_folders_street(base_dir,
     
     for file_path in tqdm(split_50p, desc="Symlinking files to 50p folder"):
         file_name = os.path.basename(file_path)
-        target_file_path = os.path.join(target_50p, file_name.replace("street", ""))
+        target_file_path = os.path.join(target_50p, file_name.replace("_street", ""))
         assert os.path.exists(file_path), f"File not found: {file_path}"
         if not os.path.exists(target_file_path):
             os.symlink(file_path, target_file_path)
@@ -252,7 +252,7 @@ def sample_and_combine_folders_street(base_dir,
     
     for file_path in tqdm(split_25p, desc="Symlinking files to 25p folder"):
         file_name = os.path.basename(file_path)
-        target_file_path = os.path.join(target_25p, file_name.replace("street", ""))
+        target_file_path = os.path.join(target_25p, file_name.replace("_street", ""))
         assert os.path.exists(file_path), f"File not found: {file_path}"
         if not os.path.exists(target_file_path):
             os.symlink(file_path, target_file_path)
@@ -263,7 +263,7 @@ def sample_and_combine_folders_street(base_dir,
     
     for file_path in tqdm(split_12p, desc="Symlinking files to 12p folder"):
         file_name = os.path.basename(file_path)
-        target_file_path = os.path.join(target_12p, file_name.replace("street", ""))
+        target_file_path = os.path.join(target_12p, file_name.replace("_street", ""))
         assert os.path.exists(file_path), f"File not found: {file_path}"
         if not os.path.exists(target_file_path):
             os.symlink(file_path, target_file_path) 
