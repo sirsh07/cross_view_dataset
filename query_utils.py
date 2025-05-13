3ea377e4e24563e8239c73c72f075cfcd5dc270d
Sirshapan Mitra <sirsh07>
2025-05-13 18:07:29 -0400

    debugged

--- query_utils.py
+++ query_utils.py
@@ -16,12 +16,21 @@ def get_test_splits(root_dir: str = "/home/zhyw86/WorkSpace/google-earth/samplin
         
         if not os.path.exists(street_split.replace("ID0001", ids)):
             print(f"street_split: {street_split.replace('ID0001', ids)} not exists")
+        else:
+            with open(street_split.replace("ID0001", ids), "r") as f:
+                lines = f.readlines()
+                if len(lines) == 0:
+                    print(f"street_split: {street_split.replace('ID0001', ids)} is empty")
+                import pdb; pdb.set_trace()
+            
         if not os.path.exists(right_split.replace("ID0001", ids)):
             print(f"right_split: {right_split.replace('ID0001', ids)} not exists")
         if not os.path.exists(middle_split.replace("ID0001", ids)):
             print(f"middle_split: {middle_split.replace('ID0001', ids)} not exists")
         if not os.path.exists(left_split.replace("ID0001", ids)):
             print(f"left_split: {left_split.replace('ID0001', ids)} not exists")
+        
+        
         
 
 if __name__ == "__main__":
