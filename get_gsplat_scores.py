576185088de917a75d5e9240f6083680a905757c
Sirshapan Mitra <sirsh07>
2025-05-13 19:02:49 -0400

    debugged

--- get_gsplat_scores.py
+++ get_gsplat_scores.py
@@ -4,13 +4,21 @@ import numpy as np
 import json
 
 
-def get_scores(dir):
+def get_scores(dir, output_file="gsplat_scores.csv"):
     
     models = []
     conds = []
     idxs = []
     splits = []
     scores = []
+    
+    psnrs = []
+    ssims = []
+    lpipss = []
+    dreamsims = []
+    num_GSs = []
+    
+    
     for root, dirs, files in os.walk(dir):
         for file in files:
             if file.endswith("val_step29999.json"):
@@ -21,16 +29,36 @@ def get_scores(dir):
                 # Extract the score from the filename
                 with open(gsplat_file, "rb") as f:
                     scores = json.load(f)
+                    
+                models.append(model)
+                conds.append(cond)
+                idxs.append(idx)
+                splits.append(split)
+                psnrs.append(scores["psnr"])
+                ssims.append(scores["ssim"])
+                lpipss.append(scores["lpips"])
+                dreamsims.append(scores["dreamsim"])
+                num_GSs.append(scores["num_GS"])
      
-                
-                import pdb; pdb.set_trace()
+    df = pd.DataFrame({
+        "model": models,
+        "cond": conds,
+        "idx": idxs,
+        "split": splits,
+        "psnr": psnrs,
+        "ssim": ssims,
+        "lpips": lpipss,
+        "dreamsim": dreamsims,
+        "num_GS": num_GSs
+    })
     
+    df.to_csv(output_file, index=False)
     
      
     
 if __name__ == "__main__":
     # Example usage
     dir = "/home/sirsh/cv_dataset/dataset_50sites/gsplat/"
-    scores = get_scores(dir)
+    scores = get_scores(dir,"./recon_scores/colmap_gsplat_scores.csv")
     print(scores) 
     
