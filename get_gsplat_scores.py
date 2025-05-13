163cf7ca09464a52afa431aa9d80aa834e43127b
Sirshapan Mitra <sirsh07>
2025-05-13 18:59:06 -0400

    debugged

--- get_gsplat_scores.py
+++ get_gsplat_scores.py
@@ -1,17 +1,27 @@
 import os
 import pandas as pd
 import numpy as np
+import json
 
 
 def get_scores(dir):
     
+    models = []
+    conds = []
+    idxs = []
+    splits = []
+    scores = []
     for root, dirs, files in os.walk(dir):
         for file in files:
             if file.endswith("val_step29999.json"):
                 gsplat_file = os.path.join(root, file)
                 
                 _, model, cond, _, idx, split, _, _, _ = gsplat_file.rsplit("/",8)
-                 
+                
+                # Extract the score from the filename
+                with open(gsplat_file, "rb") as f:
+                    scores = json.load(f)
+     
                 
                 import pdb; pdb.set_trace()
     
