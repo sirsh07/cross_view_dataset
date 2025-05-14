ecce3c5fef848252d4ad39ddcecd7c5469186596
Sirshapan Mitra <sirsh07>
2025-05-14 00:47:04 -0400

    debugged

--- get_gsplat_scores.py
+++ get_gsplat_scores.py
@@ -24,7 +24,7 @@ def get_scores(dir, output_file="gsplat_scores.csv"):
             if file.endswith("val_step29999.json"):
                 gsplat_file = os.path.join(root, file)
                 
-                # import pdb; pdb.set_trace()
+                import pdb; pdb.set_trace()
                 
                 _, model, cond, _, idx, split, _, _, _ = gsplat_file.rsplit("/",8)
                 
@@ -66,7 +66,10 @@ if __name__ == "__main__":
     # scores = get_scores(dir,"./recon_scores/colmap_gsplat_scores.csv")
     
     # dir = "/home/sirsh/cv_dataset/dataset_50sites/gsplat_mast3r"
-    dir = "/home/smitra/cv_dataset/dataset_50sites/gsplat"
+    # dir = "/home/smitra/cv_dataset/dataset_50sites/gsplat"
+    # scores = get_scores(dir,"./recon_scores/mast3r_gsplatmcmc_scores.csv")
+    
+    dir = "/home/smitra/cv_dataset/dataset_50sites/gsplat_mcmc_mast3r_sfm"
     scores = get_scores(dir,"./recon_scores/mast3r_gsplatmcmc_scores.csv")
     
     print(scores) 
